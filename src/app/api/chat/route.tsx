fbd0e25ec53811f180076c6a71d23a80d375d895
Felipe Ramos <129113618+fliperamos88@users.noreply.github.com>
2025-11-18 18:01:39 -0500

    updated model

--- src/app/api/chat/route.tsx
+++ src/app/api/chat/route.tsx
@@ -11,7 +11,7 @@ export async function POST(request: NextRequest) {
     const combinedPrompt = `${basePrompt} ${prompt}`;
 
     const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-    const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+    const model = genAI.getGenerativeModel({ model: 'gemini-2.5-flash' });
     const result = await model.generateContent(combinedPrompt);
     const response = result.response;
     const text = response.text().replaceAll('*', '');
